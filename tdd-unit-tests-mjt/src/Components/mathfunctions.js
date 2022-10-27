bff9ba614d0b9d0e7f2099d1f45b77532d5c3718
Michael Thompson <thompsonmichaelj2020@gmail.com>
2022-10-27 16:24:50 -0400

    Mathfunctions complete

--- tdd-unit-tests-mjt/src/Components/mathfunctions.js
+++ tdd-unit-tests-mjt/src/Components/mathfunctions.js
@@ -1,5 +1,18 @@
 
 function sum(numOne, numTwo) {
-    
+    return numOne + numTwo; //Write this LAST
 }
-export default sum;
+
+function difference(numOne, numTwo) {
+    return numOne - numTwo;  //Write this LAST
+}
+
+function product(numOne, numTwo) {
+    return numOne * numTwo; //This is LAST
+}
+
+function quotient(numOne, numTwo) {
+    return numOne / numTwo; // This is LAST
+}
+
+export { sum, difference, product, quotient };
--- tdd-unit-tests-mjt/src/Components/mathfunctions.test.js
+++ tdd-unit-tests-mjt/src/Components/mathfunctions.test.js
@@ -1,7 +1,58 @@
-import sum from './mathfunctions'
+import { sum, difference, product, quotient } from './mathfunctions'
 
-test('adds 2 + 2 to equal 4', () => {
-    let value = sum(2, 2)
-    expect(value).toBe(4)
+describe('test sum function of 4 + 3', () => {
+    let value = sum (4,3)
 
+    it('should equal 7', () => {        
+        expect(value).toBe(7);
+    })
+    it('should be greater than 3', () => {
+        expect(value).toBeGreaterThan(3)
+    })
+    it('should not be less than 0', () => {
+        expect(value).not.toBeLessThan(0)
+    })
+})
+
+describe('test subtraction function of 9 - 5', () => {
+    let value = difference(9, 5)
+
+    it('should equal 4', () => {
+        expect(value).toBe(4);
+    })
+    it('should be greater than 3', () => {
+        expect(value).toBeGreaterThan(3)
+    })
+    it('should not be less than 0', () => {
+        expect(value).not.toBeLessThan(0)
+    })
+})
+
+describe('test multiplication function of -7 x 3', () => {
+    let value = product(-7, 3)
+
+    it('should equal -21', () => {
+        expect(value).toBe(-21);
+    })
+    it('should be greater than -22', () => {
+        expect(value).toBeGreaterThan(-22)
+    })
+    it('should not be greater than 0', () => {
+        expect(value).not.toBeGreaterThan(0)
+    })
+})
+
+
+describe('test division function of 21 / 3', () => {
+    let value = quotient(21, 3)
+
+    it('should equal 7', () => {
+        expect(value).toBe(7);
+    })
+    it('should NOT be less than 22', () => {
+        expect(value).not.toBeGreaterThan(22)
+    })
+    it('should be greater than 0', () => {
+        expect(value).toBeGreaterThan(0)
+    })
 })
